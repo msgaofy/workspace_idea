c2a8380a23c17282e51a9d37b03a4ee8453f0ea8
msgaofy <msgaofy@163.com>
2018-05-03 08:28:13 +0800

    test idea2018.1.2

--- cbx-ykt/ykt-01/src/test/java/com/cbx/ykt/tlv/tset/TLVTest.java
+++ cbx-ykt/ykt-01/src/test/java/com/cbx/ykt/tlv/tset/TLVTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class TLVTest {
+
     public static void main(String[] args) {
         String data = "950500000008004F08A0000003330101019F36022935500A50424F432044454249549F3704CC8DEE7A82027C009F34030000009F101307020103A00000010A010000017400116766E99F3303E0E1C0";
         List<TLVMessage> tlvMessageList =  TLVUtils.builderTlvList(data);
